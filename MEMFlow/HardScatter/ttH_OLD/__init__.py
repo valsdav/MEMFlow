d814f9f333f9a6e64725cac6806dda7f05506f39
Davide Valsecchi <valsecchi.davide94@gmail.com>
2022-12-19 17:23:22 +0100

    Moved src

--- MEMFlow/HardScatter/ttH_OLD/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .all_matrix2py import *
-from .P1_gg_httx2py import *
-from .P1_uux_httx2py import *
---
+++ src/memflow/hardscatter/ttH/allmatrix2py.py
@@ -0,0 +1,12 @@
+try:
+    from P1_gg_httx2py import *
+except:
+    from .P1_gg_httx2py import *
+try:
+    from P1_uux_httx2py import *
+except:
+    from .P1_uux_httx2py import *
+try:
+    from all_matrix2py import *
+except:
+    from .all_matrix2py import *
