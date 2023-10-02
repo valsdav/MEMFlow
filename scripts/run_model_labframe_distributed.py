0f898b3801f7d8c39bb28e09df763265b0ee1b0c
Davide Valsecchi <davide.valsecchi@cern.ch>
2023-10-02 12:11:58 +0200

    Dumping configs for flow

--- scripts/run_model_labframe_distributed.py
+++ scripts/run_model_labframe_distributed.py
@@ -312,6 +312,17 @@ def train( device, name_dir, config,  outputDir, dtype,
                                                               threshold=config.training_params.reduce_on_plateau.threshold,
                                                                min_lr=config.training_params.reduce_on_plateau.get("min_lr", 1e-7),
                                                                verbose=True)
+    elif scheduler_type == "cyclic_lr":
+        scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer,
+                                                     base_lr= config.training_params.cyclic_lr.base_lr,
+                                                     max_lr= config.training_params.cyclic_lr.max_lr,
+                                                     step_size_up=config.training_params.cyclic_lr.step_size_up,
+                                                     step_size_down=None,
+                                                      cycle_momentum=False,
+                                                     gamma=config.training_params.cyclic_lr.gamma,
+                                                     mode=config.training_params.cyclic_lr.mode,
+                                                      verbose=False)
+        
     
     
     early_stopper = EarlyStopper(patience=config.training_params.nEpochsPatience, min_delta=0.0001)
@@ -399,6 +410,9 @@ def train( device, name_dir, config,  outputDir, dtype,
             optimizer.step()
             sum_loss += loss_final.item()
 
+            if scheduler_type == "cyclic_lr": #cycle each step
+                scheduler.step()
+
             with torch.no_grad():
                 if exp is not None and device==0 or world_size is None:
                     if i % config.training_params.interval_logging_steps == 0:
@@ -437,14 +451,15 @@ def train( device, name_dir, config,  outputDir, dtype,
                         exp.log_metric('flow_nproblems', mask_problematic.sum(), step=ii )
                         # Log the average difference of ps points
                         exp.log_metric('train_PSregr_avgdiff', (logit_ps_regr - ps_target_scaled).abs().mean(), step=ii)
-                        exp.log_metric('val_PSregr_stddiff', (logit_ps_regr - ps_target_scaled).std().mean(), step=ii)
+                        exp.log_metric('train_PSregr_stddiff', (logit_ps_regr - ps_target_scaled).std().mean(), step=ii)
                         exp.log_metric('train_PSregr_mmd', MMD(logit_ps_regr, ps_target_scaled, config.training_params.mmd_kernel, device, dtype), step=ii)
-                
+                        exp.log_metric("learning_rate", optimizer.param_groups[0]['lr'], step=ii)
 
         ### END of training 
         if exp is not None and device==0 or world_size is None:
             exp.log_metric("loss_epoch_total_train", sum_loss/N_train, epoch=e, step=ii)
-            exp.log_metric("learning_rate", optimizer.param_groups[0]['lr'], epoch=e, step=ii)
+            if scheduler_type != "cyclic_lr":
+                exp.log_metric("learning_rate", scheduler.get_last_lr(), epoch=e, step=ii)
 
         
         valid_loss_huber = 0.
@@ -609,27 +624,33 @@ def train( device, name_dir, config,  outputDir, dtype,
 
                     # Sampling
                     N_samples = config.training_params.sampling_points
-                    ps_samples = model.flow(flow_cond_vector).sample((N_samples,)).cpu()
-                    for x in range(10):
+                    ps_samples = model.flow(flow_cond_vector).sample((N_samples,))
+                    for j in range(10):
                         fig, ax = plt.subplots(figsize=(7,6), dpi=100)
-                        h = ax.hist2d(ps_target_scaled.cpu()[:,x].tile(N_samples,1,1).flatten().numpy(),
-                                        ps_samples[:,:,x].flatten().numpy(),
+                        h = ax.hist2d(ps_target_scaled[:,j].tile(N_samples,1,1).flatten().cpu().numpy(),
+                                        ps_samples[:,:,j].flatten().cpu().numpy(),
                                         bins=50, range=((-1, 1),(-1, 1)),cmin=1)
                         fig.colorbar(h[3], ax=ax)
-                        exp.log_figure(f"ps_sampled_2D_{x}", fig, step=e)
+                        exp.log_figure(f"ps_sampled_2D_{j}", fig, step=e)
 
                         fig, ax = plt.subplots(figsize=(7,6), dpi=100)
-                        h = ax.hist2d(ps_target_scaled.cpu()[:,x].tile(N_samples,1,1).flatten().numpy(),
-                                        ps_samples[:,:,x].flatten().numpy(),
+                        h = ax.hist2d(ps_target_scaled[:,j].tile(N_samples,1,1).flatten().cpu().numpy(),
+                                        ps_samples[:,:,j].flatten().cpu().numpy(),
                                         bins=50, range=((-1, 1),(-1, 1)), norm=LogNorm() )
                         fig.colorbar(h[3], ax=ax)
-                        exp.log_figure(f"ps_sampled_2D_{x}_log", fig, step=e)
+                        exp.log_figure(f"ps_sampled_2D_{j}_log", fig, step=e)
 
                         fig, ax = plt.subplots(figsize=(7,6), dpi=100)
                         h = ax.hist(
-                            (ps_target_scaled.cpu()[:,x].tile(N_samples,1,1) - ps_samples[:,:,x]).flatten().numpy(),
+                            (ps_target_scaled[:,j].tile(N_samples,1,1) - ps_samples[:,:,j]).flatten().cpu().numpy(),
                             range=(-1,1),   bins=50)
-                        exp.log_figure(f"ps_1D_{x}", fig, step=e)
+                        exp.log_figure(f"ps_1D_{j}", fig, step=e)
+
+                        # Correlation coefficiency for each sampled PS
+                        # Just take the first sample
+                        exp.log_metric(f"val_ps_sampled_corrcoef_{j}",
+                                       torch.corrcoef(torch.stack((ps_samples[0,:,j], ps_target_scaled[:,j]))), epoch=e)
+
                     
 
         if exp is not None and device==0 or world_size is None:
