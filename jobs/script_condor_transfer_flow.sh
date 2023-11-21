929ae8244069eaa70aa25ab3185f73e76f887a74
Antonio Petre <adrian_antonio99@yahoo.ro>
2023-11-21 10:42:40 +0100

    Fix again submit

--- jobs/script_condor_transfer_flow.sh
+++ jobs/script_condor_transfer_flow.sh
@@ -12,7 +12,7 @@ if [ "${@: -1}" = "$ANTONIOVAR" ]; then
        pip install -e .
 
        python scripts/run_transferFlow_firstVersion.py \
-              --config  /afs/cern.ch/user/a/adpetre/public/memflow/MEMFlow/configs/$1 \
+              --path-config  /afs/cern.ch/user/a/adpetre/public/memflow/MEMFlow/configs/$1 \
               --output-dir  /eos/user/a/adpetre/www/ttHbbAnalysis/MEMFlow/models_archive/$2 \
               --on-GPU
 else
